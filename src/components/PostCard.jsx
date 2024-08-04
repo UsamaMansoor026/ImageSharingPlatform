57c636c8c612d80125bd236e627939a6b63b8e5c
usama mansoor <maan026@gmail.com>
2024-08-04 10:16:54 +0500

    delete post functionality added

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import { db } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { toast } from "react-toastify";
 
-const PostCard = ({ item, index }) => {
+const PostCard = ({ item, index, handleDelete }) => {
   const navigate = useNavigate();
+  const { JSONData } = useContext(UserContext);
+  const user = JSONData();
+
+  /* Handle Delete */
+  /*  const handleDelete = async (id) => {
+    console.log(id);
+    await deleteDoc(doc(db, "posts", id))
+      .then(() => toast.success("Post deleted successfully"))
+      .catch((err) => {
+        console.log(err);
+        toast.error("An error occurred while deleting");
+      });
+  }; */
+
   return (
     <article className="postCard" key={index}>
       {/* Author Details */}
@@ -13,6 +31,13 @@ const PostCard = ({ item, index }) => {
           alt=""
         />
         <h3>{item.authorname}</h3>
+        {item.authorid === user.uid ? (
+          <span onClick={() => handleDelete(item.id)} className="trash">
+            <ion-icon name="trash-outline"></ion-icon>
+          </span>
+        ) : (
+          <></>
+        )}
       </div>
       <hr className="hr" />
       <p className="postCaption">{item.caption}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,28 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../firebase";
 import PostCard from "../components/PostCard";
+import { toast } from "react-toastify";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const postsCollectionRef = collection(db, "posts");
 
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "posts", id))
+      .then(() => toast.success("Post deleted successfully"))
+      .catch((err) => {
+        console.log(err);
+        toast.error("An Error occured while deleting");
+      });
+
+    getAllPostsFromDB();
+  };
+
   const getAllPostsFromDB = async () => {
     const querySnapshot = await getDocs(postsCollectionRef);
     const allPosts = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
       authorid: doc.data().authorid,
       authorname: doc.data().authorname,
       authorprofile: doc.data().authorprofile,
@@ -29,8 +42,8 @@ const Home = () => {
       <h3 className="section_heading">Latest Posts:</h3>
 
       <div className="post_container">
-        {posts.map((item, index) => (
-          <PostCard index={index} item={item} />
+        {posts.map((item) => (
+          <PostCard index={item.id} item={item} handleDelete={handleDelete} />
         ))}
       </div>
     </section>
